1c5a3ef6b0826097566f5064833a27a5ccc5ac64
horietakehiro <nashhoward1312@docomo.ne.jp>
2020-06-16 23:42:52 +0900

    run toy program

--- codes/lesser-pandas/src/statistics/describe.go
+++ codes/lesser-pandas/src/statistics/describe.go
@@ -1,9 +1,21 @@
 package main
 
 import (
+	"io"
+	"os"
+	"encoding/csv"
 	"fmt"
 )
 
 func main()  {
-	
+	reader, _ := os.Open("sample.csv")
+	r := csv.NewReader(reader)
+	for {
+		line, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		fmt.Println(line)
+
+	}
 }
