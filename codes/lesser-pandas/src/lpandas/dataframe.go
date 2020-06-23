0fb10adc3fba505bd8a4136ed335703083859531
horietakehiro <nashhoward1312@docomo.ne.jp>
2020-06-23 22:33:52 +0900

    Describe method stdout count,mean,max,min,sum,std

--- codes/lesser-pandas/src/lpandas/dataframe.go
+++ codes/lesser-pandas/src/lpandas/dataframe.go
@@ -5,7 +5,9 @@ import (
 	"os"
 	"encoding/csv"
 	"strconv"
+	"strings"
 	"io"
+	"math"
 )
 
 // DataFrame is a struct for storing structured data, like csv.
@@ -69,13 +71,80 @@ func (df *DataFrame) ReadCsv(filePath string) {
 // count, mean, max, min, 25percentile, 50percentile, 75percentile
 // via column-wise
 func (df *DataFrame) Describe() {
+	counts := make([]float64, df.Shape[1])
+	sums := make([]float64, df.Shape[1])
 	means := make([]float64, df.Shape[1])
-	mean := float64(0)
+	maxes := make([]float64, df.Shape[1])
+	mins := make([]float64, df.Shape[1])
+	stds := make([]float64, df.Shape[1])
+
+
 	for i := 0; i < df.Shape[1]; i++ {
+		// count := float64(0)
+		mean := float64(0)
+		sum := float64(0)
+		max := float64(0)
+		min := float64(0)
+		std := float64(0)
+
 		for j := 0; j < df.Shape[0]; j++ {
-			mean += df.Rows[j][i]
+			val := df.Rows[j][i]
+			sum += val
+			mean += val
+			// initialize max, min
+			if j == 0 {
+				max = val
+				min = val
+			}
+
+			if max < val {
+				max = val
+			}
+			if min > val {
+				min = val
+			}
 		}
+		// calc standard deviation using mean
+		sigmaSquared := float64(0)
+		for j := 0; j < df.Shape[0]; j++ {
+			sigmaSquared += math.Pow(df.Rows[j][i] - (mean / float64(df.Shape[0])), 2)
+		}
+		std = math.Sqrt(sigmaSquared / float64(df.Shape[0]))
+
+		counts[i] = float64(df.Shape[0])
+		sums[i] = sum
 		means[i] = mean / float64(df.Shape[0])
+		maxes[i] = max
+		mins[i] = min
+		stds[i] = std
 	}
-	fmt.Printf("%v", means[0])
+	outputDescribe(df, counts, sums, means, maxes, mins, stds)
+}
+
+func outputDescribe(df *DataFrame, counts, sums, means, maxes, mins, stds []float64) {
+	strCounts := make([]string, len(counts))
+	strSums := make([]string, len(sums))
+	strMeans := make([]string, len(means))
+	strMaxes := make([]string, len(maxes))
+	strMins := make([]string, len(mins))
+	strStds := make([]string, len(stds))
+
+
+	for i := 0; i < df.Shape[0]; i++ {
+		strCounts[i] = fmt.Sprintf("%.3f", counts[i])
+		strSums[i] = fmt.Sprintf("%.3f", sums[i])
+		strMeans[i] = fmt.Sprintf("%.3f", means[i])
+		strMaxes[i] = fmt.Sprintf("%.3f", maxes[i])
+		strMins[i] = fmt.Sprintf("%.3f", mins[i])
+		strStds[i] = fmt.Sprintf("%.3f", stds[i])
+	}
+
+	fmt.Printf("metric,%s\n",strings.Join(df.Columns, ","))
+	fmt.Printf("count,%s\n",strings.Join(strCounts, ","))
+	fmt.Printf("sum,%s\n",strings.Join(strSums, ","))
+	fmt.Printf("mean,%s\n",strings.Join(strMeans, ","))
+	fmt.Printf("max,%s\n",strings.Join(strMaxes, ","))
+	fmt.Printf("min,%s\n",strings.Join(strMins, ","))	
+	fmt.Printf("std,%s\n",strings.Join(strStds, ","))
+
 }
--- codes/lesser-pandas/src/lpandas/dataframe_test.go
+++ codes/lesser-pandas/src/lpandas/dataframe_test.go
@@ -13,8 +13,11 @@ func TestDataFrame1(t *testing.T)  {
 
 	correctCsvData := lpandas.DataFrame{
 		Columns : []string{"f1", "f2", "f3"},
-		Rows : [][]float64{{float64(1), float64(2), float64(3)},
-							{float64(0.5), float64(10), float64(0)}},
+		Rows : [][]float64{
+			{float64(1), float64(2), float64(3)},
+			{float64(0.5), float64(10), float64(0)},
+			{float64(0.75), float64(0), float64(0)},
+		},
 	}
 
 	assert.ElementsMatch(t, csvData.Columns, correctCsvData.Columns,
@@ -36,7 +39,14 @@ func TestDataFrame2(t *testing.T)  {
 func ExampleDataFrame() {
 	csvData := PrepareDataFrame4Test()
 	csvData.Describe()
-	// Output: 0.75
+	// Output:
+	// metric,f1,f2,f3
+	// count,3.000,3.000,3.000
+	// sum,2.250,12.000,3.000
+	// mean,0.750,4.000,1.000
+	// max,1.000,10.000,3.000
+	// min,0.500,0.000,0.000
+	// std,0.204,4.320,1.414
 }
 
 func PrepareDataFrame4Test() lpandas.DataFrame {
