03c521e3f48dc6e6543474646eb4ed4b4847e176
horietakehiro <nashhoward1312@docomo.ne.jp>
2020-06-29 23:08:39 +0900

    remove unneeded codes

--- codes/lesser-pandas/src/lpandas/dataframe.go
+++ codes/lesser-pandas/src/lpandas/dataframe.go
@@ -6,9 +6,7 @@ import (
 	"encoding/csv"
 	"strconv"
 	"reflect"
-	"sort"
 	"strings"
-	// "io"
 	"math"
 
 	"lpandas/helper"
@@ -177,11 +175,6 @@ func (df *DataFrame) Info() {
 // StringColumns : count, unique, top, freq
 func (df *DataFrame) Describe() {
 
-	// numericMetric := []string{"count", "mean", "std", "min", "25%", "50%", "75%", "max", "sum"}
-
-	// stringIntMetric := []string{"count", "unique", "freq"}
-	// stringStrMetric := []string{"top"}
-
 	count := "count" + ","
 	mean := "mean" + ","
 	std := "std" + ","
@@ -223,17 +216,6 @@ func (df *DataFrame) Describe() {
 	fmt.Println(sum)
 
 
-	// for _, key := range numericMetric {
-	// 	stdoutString := key + ","
-	// 	for j, col := range orderedNumericColumns {
-	// 		stdoutString += fmt.Sprintf("%.3f", numericStatistics[key][col])
-	// 		if j < len(orderedNumericColumns) - 1 {
-	// 			stdoutString += ","
-	// 		}
-	// 	}
-	// 	fmt.Println(stdoutString)
-	// }
-
 	fmt.Println("")
 	count = "count" + "," // reuse 
 
@@ -254,29 +236,6 @@ func (df *DataFrame) Describe() {
 	fmt.Println(freq)
 	fmt.Println(top)
 
-
-	// for _, key := range orderedStringIntMetric {
-	// 	stdoutString := key + ","
-	// 	for j, col := range orderedStringColumns {
-	// 		stdoutString += fmt.Sprintf("%d", stringIntStatistics[key][col])
-	// 		if j < len(orderedStringColumns) - 1 {
-	// 			stdoutString += ","
-	// 		}
-	// 	}
-	// 	fmt.Println(stdoutString)
-	// }
-	// for _, key := range orderedStringStrMetric {
-	// 	stdoutString := key + ","
-	// 	for j, col := range orderedStringColumns {
-	// 		stdoutString += fmt.Sprintf("%s", stringStrStatistics[key][col])
-	// 		if j < len(orderedStringColumns) - 1 {
-	// 			stdoutString += ","
-	// 		}
-	// 	}
-	// 	fmt.Println(stdoutString)
-	// }
-
-	
 }
 
 func (df *DataFrame) createNumericResult(i int, metric string, result float64) string {
@@ -301,207 +260,4 @@ func (df *DataFrame) createStringStrResult(i int, metric string, result string) 
 		metric += ","
 	}
 	return metric
-}
-
-func calclNumericStatistics(df *DataFrame) map[string]map[string]float64 {
-	// e.g. {"count" : {"Age" : 100, "..." : ...}, ...}
-	numericStatistics := map[string]map[string]float64{
-		"count" : map[string]float64{},
-		"mean" : map[string]float64{},
-		"std" : map[string]float64{},
-		"min" : map[string]float64{},
-		"25%" : map[string]float64{},
-		"50%" : map[string]float64{},
-		"75%" : map[string]float64{},
-		"max" : map[string]float64{},
-		"sum" : map[string]float64{},
-	}
-
-	// initiate maps 
-	for _, val := range numericStatistics {
-		for key := range df.Numeric {
-			val[key] = 0.0
-		}
-	}
-
-	for key, values := range df.Numeric {
-		// firstlly, sort acsendingly
-		sort.Sort(helper.AcsendingSort(values))
-
-		calcFlag := true
-		for i, val := range df.Numeric[key] {
-			if math.IsNaN(val) {
-				continue
-			}
-
-			// sum
-			numericStatistics["sum"][key] += val
-
-			// min, count
-			if calcFlag {
-				numericStatistics["min"][key] = val
-				numericStatistics["count"][key] = float64(df.NumericShape[0] - i)
-				calcFlag = false
-				continue
-			}
-
-		}
-		// max
-		numericStatistics["max"][key] = df.Numeric[key][df.NumericShape[0]-1]
-		// mean
-		numericStatistics["mean"][key] = numericStatistics["sum"][key] / numericStatistics["count"][key]
-		// std
-		sigmaSquared := float64(0)
-		for _, val := range df.Numeric[key] {
-			if !math.IsNaN(val) {
-				sigmaSquared += math.Pow(val -
-								 numericStatistics["mean"][key], 2)
-			}
-		}
-		numericStatistics["std"][key] = math.Sqrt(
-			sigmaSquared / numericStatistics["count"][key])
-
-		// percentiles
-		numericStatistics["25%"][key] = calcPercentile(df.Numeric[key], 0.25)
-		numericStatistics["50%"][key] = calcPercentile(df.Numeric[key], 0.50)
-		numericStatistics["75%"][key] = calcPercentile(df.Numeric[key], 0.75)
-		
-	}
-
-	return numericStatistics
-
-}
-
-func calcStringStatistics(df *DataFrame) (map[string]map[string]int, map[string]map[string]string) {
-	stringIntStatistics := map[string]map[string]int{
-		"count" : map[string]int{},
-		"unique" : map[string]int{},
-		"freq" : map[string]int{},
-	}
-	stringStrStatistics := map[string]map[string]string {
-		"top" : map[string]string{},
-	}
-
-	// initiate maps 
-	for key := range df.String {
-		for _, val := range stringIntStatistics {
-			val[key] = 0
-		}
-		for _, val := range stringStrStatistics {
-			val[key] = ""
-		}
-	}
-
-	for key, values := range df.String {
-		sort.Strings(values)
-		calcFlag := true
-		tmpCounter := map[string]int{}
-
-		for i, val := range df.String[key] {
-			if val == "" {
-				continue
-			}
-
-			// count (exclude empty entry : "" )
-			if calcFlag {
-				stringIntStatistics["count"][key] = df.StringShape[0] - i
-				calcFlag = false
-			}
-
-			// for unique, top, freq
-			tmpCounter[val]++
-
-		}
-
-		// unique, top, frep
-
-		stringIntStatistics["unique"][key] = len(tmpCounter)
-		mostCommonKeys, mostCommonValues := helper.PythonicStrCounterMostCommon(
-			tmpCounter, 1)
-		stringIntStatistics["freq"][key] = mostCommonValues[0]
-
-		stringStrStatistics["top"][key] = mostCommonKeys[0]
-
-
-	}
-
-	return stringIntStatistics, stringStrStatistics
-
-}
-
-func calcPercentile(values []float64, percentile float64) float64 {
-	nonNullValues := []float64{}
-	for _, val := range values {
-		if !math.IsNaN(val) {
-			nonNullValues = append(nonNullValues, val)
-		}
-	}
-	N := len(nonNullValues) - 1 // this is not a length, but distance from head to tail
-	p := float64(N) * percentile
-	q := int(math.Floor(p))
-	r := p - float64(q)
-	D := nonNullValues[q] + (nonNullValues[q+1] - nonNullValues[q]) * r // linear interporation
-	return D
-}
-
-func stdoutDescribe(df *DataFrame, 
-	numericStatistics map[string]map[string]float64,
-	stringIntStatistics map[string]map[string]int,
-	stringStrStatistics map[string]map[string]string) {
-
-	orderedNumericMetric := []string{"count", "mean", "std", "min", "25%", "50%", "75%", "max", "sum"}
-	orderedNumericColumns := []string{}
-	for key := range df.Numeric {
-		orderedNumericColumns = append(orderedNumericColumns, key)
-	}
-	sort.Strings(orderedNumericColumns)
-
-	orderedStringIntMetric := []string{"count", "unique", "freq"}
-	orderedStringStrMetric := []string{"top"}
-	orderedStringColumns := []string{}
-	for key := range df.String {
-		orderedStringColumns = append(orderedStringColumns, key)
-	}
-	sort.Strings(orderedStringColumns)
-
-
-
-	fmt.Printf("=== Numeric columns (total %d columns) =====\n", len(orderedNumericColumns))
-	fmt.Printf("metric,%s\n", strings.Join(orderedNumericColumns, ","))
-	for _, key := range orderedNumericMetric {
-		stdoutString := key + ","
-		for j, col := range orderedNumericColumns {
-			stdoutString += fmt.Sprintf("%.3f", numericStatistics[key][col])
-			if j < len(orderedNumericColumns) - 1 {
-				stdoutString += ","
-			}
-		}
-		fmt.Println(stdoutString)
-	}
-
-	fmt.Println("")
-
-	fmt.Printf("=== String columns (total %d columns) =====\n", len(orderedStringColumns))
-	fmt.Printf("metric,%s\n", strings.Join(orderedStringColumns, ","))
-	for _, key := range orderedStringIntMetric {
-		stdoutString := key + ","
-		for j, col := range orderedStringColumns {
-			stdoutString += fmt.Sprintf("%d", stringIntStatistics[key][col])
-			if j < len(orderedStringColumns) - 1 {
-				stdoutString += ","
-			}
-		}
-		fmt.Println(stdoutString)
-	}
-	for _, key := range orderedStringStrMetric {
-		stdoutString := key + ","
-		for j, col := range orderedStringColumns {
-			stdoutString += fmt.Sprintf("%s", stringStrStatistics[key][col])
-			if j < len(orderedStringColumns) - 1 {
-				stdoutString += ","
-			}
-		}
-		fmt.Println(stdoutString)
-	}
-
 }
